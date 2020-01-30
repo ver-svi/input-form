e948711fc0d4e0ef77d735901ff044eac0e5bf34
ver-svi <steve_daskal@yahoo.com>
2020-01-30 11:30:08 +0200

    vavejdam inputform validation

--- index.php
+++ index.php
@@ -1,20 +1,82 @@
-inputform>
-vercheto
+<?php
+    // Variable to check
+    $url = "https://www.w3schools.com";
+
+    // Validate url
+    if (filter_var($url, FILTER_VALIDATE_URL)) {
+        echo("$url is a valid URL");
+    } else {
+        echo("$url is not a valid URL");
+    }
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>URL Validator</title>
+</head>
 <body>
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>  
 
 <?php
-// Variable to check
-$url = "https://www.w3schools.com";
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
 
-// Validate url
-if (filter_var($url, FILTER_VALIDATE_URL)) {
-    echo("$url is a valid URL");
-} else {
-    echo("$url is not a valid URL");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
 ?>
 
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
 </body>
 </html>
