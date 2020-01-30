df90545e2636436e2f28016c5baea2a99353d40d
ver-svi <steve_daskal@yahoo.com>
2020-01-30 11:09:03 +0200

    primer w3scools input form

---
+++ index.php
@@ -0,0 +1,20 @@
+inputform>
+vercheto
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Variable to check
+$url = "https://www.w3schools.com";
+
+// Validate url
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+    echo("$url is a valid URL");
+} else {
+    echo("$url is not a valid URL");
+}
+?>
+
+</body>
+</html>
