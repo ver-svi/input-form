6dcc546d5f7bbfdfb257c6c7540ce9f1505f635e
ver-svi <steve_daskal@yahoo.com>
2020-01-31 12:54:26 +0200

    checked Gender

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     Comment: <textarea name="comment" rows="5" cols="40"></textarea>
     <br><br>
     Gender:
-    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="female" checked>Female
     <input type="radio" name="gender" value="male">Male
     <input type="radio" name="gender" value="other">Other
     <br><br>
@@ -59,7 +59,4 @@ echo "<br>";
 echo $comment;
 echo "<br>";
 echo $gender;
-?>
-<?php
-var_dump($_GET);
 ?>
